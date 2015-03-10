9a3a75fd9a39dced81aa1cd35f9f49cd36a3d71d
dicklong <longjun91@qq.com>
2015-03-10 16:47:55 +0800

    第二次commit 新建了HelloGit.java类

---
+++ HelloGit/src/com/dyr/test/HelloGit.java
@@ -0,0 +1,11 @@
+package com.dyr.test;
+/*
+ *www.dyr.com
+ *Copyright (c) 2014 All Right Reserved.
+ */
+
+public class HelloGit {
+		public static void main(String[] args) {
+			System.out.println("Hello Git£¡£¡");
+		}
+}
